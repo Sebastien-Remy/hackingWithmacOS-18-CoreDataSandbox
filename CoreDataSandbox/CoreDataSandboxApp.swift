2399683d809ff28bf4e6a6a6afa156308cd71289
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-05 17:42:28 +0100

    Initial Commit

---
+++ CoreDataSandbox/CoreDataSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataSandboxApp.swift
+//  CoreDataSandbox
+//
+//  Created by Sebastien REMY on 05/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
