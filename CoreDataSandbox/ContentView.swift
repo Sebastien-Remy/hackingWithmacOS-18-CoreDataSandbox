b442dd47864f18add59cb9337c06af50a8acd433
Sébastien REMY <55869882+Sebastien-Remy@users.noreply.github.com>
2022-11-06 07:24:50 +0100

    Filtering

--- CoreDataSandbox/ContentView.swift
+++ CoreDataSandbox/ContentView.swift
@@ -5,27 +5,47 @@
 //  Created by Sebastien REMY on 05/11/2022.
 //
 
+import CoreData
 import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var managedObjectContext
-    @FetchRequest(sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(sortDescriptors: []
+//                  predicate: NSPredicate(format: "universe ==  %@", "Star Wars")
+//                  predicate: NSPredicate(format: "name <  %@", "F")
+//                  predicate: NSPredicate(format: "universe IN %@", ["Alien", "Firefly", "Star Trek"])
+    ) var ships: FetchedResults<Ship>
     
     var body: some View {
         VStack {
-            List(wizards) { wizard in
-                Text(wizard.name ?? "unknown")
+            List(ships) { ship in
+                Text(ship.name ?? "unknown")
             }
-            Button("Add") {
-                let wizar = Wizard(context: managedObjectContext)
-                wizar.name = "Harry Potter"
+            Button("Show Star Wars") {
+                ships.nsPredicate = NSPredicate(format: "universe == %@", "Star Wars")
             }
-            Button("Save") {
-                do {
-                    try managedObjectContext.save()
-                } catch {
-                    print(error.localizedDescription)
-                }
+            Button("Show Star Trek") {
+                ships.nsPredicate = NSPredicate(format: "universe == %@", "Star Trek")
+            }
+            Button("Add exemple") {
+                let ship1 = Ship(context: managedObjectContext)
+                ship1.name = "Entreprise"
+                ship1.universe = "Star Trek"
+                
+                let ship2 = Ship(context: managedObjectContext)
+                ship2.name = "Defiant"
+                ship2.universe = "Star Trek"
+                
+                let ship3 = Ship(context: managedObjectContext)
+                ship3.name = "Millenium Falcon"
+                ship3.universe = "Star Wars"
+                
+                let ship4 = Ship(context: managedObjectContext)
+                ship4.name = "Executor"
+                ship4.universe = "Star Wars"
+                
+                try? managedObjectContext.save()
+                
             }
         }
     }
